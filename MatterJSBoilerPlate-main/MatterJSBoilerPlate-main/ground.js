eaec716651628ab4f2eabfdd12d9b23180c241dc
JC-HL <103821699+JC-HL@users.noreply.github.com>
2022-07-15 19:02:37 +0530

    Add files via upload

---
+++ MatterJSBoilerPlate-main/MatterJSBoilerPlate-main/ground.js
@@ -0,0 +1,25 @@
+class Ground{
+    constructor(x, y, w, h){
+        this.x= x;
+        this.y= y;
+        this.w= w;
+        this.h= h;
+       var ground_options={
+        isStatic: true
+       }
+this.body= Bodies.rectangle(x, y, w, h, ground_options)
+World.add(world, this.body)
+    }
+
+display(){
+    var pos= this.body.position;
+    push()
+    translate(pos.x, pos.y)
+    rectMode(CENTER);
+    fill("green")
+    rect(0, 0, this.width, this.height)
+    pop()
+}
+
+}
+
